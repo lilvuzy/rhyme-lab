e8c33ff3d66589c52d494d2ce70598855537c25a
lilvuzy <112009814+lilvuzy@users.noreply.github.com>
2022-11-10 17:03:41 -0500

    displaying still need to edit sizing

--- script.js
+++ script.js
@@ -1,59 +1,93 @@
-// get relevant dom elements
-const searchBox = document.getElementById("query");
+// const query = "hello";
 
-const searchForm = document.getElementById("search-form");
+const searchBoxElem = document.getElementById("query");
+const resultsContainerElem = document.getElementById("results");
 
-const resultDiv = document.getElementById("results");
+// when someone presses enter in the search box,
+searchBoxElem.addEventListener("keydown", whenSomeKeyPressed);
 
-// add event listener to know when to search
-
-searchBox.addEventListener("keyup", onPress);
-
-async function onPress(event) {
+async function whenSomeKeyPressed(event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    // clearResults();
-
-    console.log(searchBox.value);
-    const rhymeResults = await search(searchBox.value);
-
-    const rhymeElements = await createElements(rhymeResults);
+    const rhymes = await searchForRhymes(searchBoxElem.value);
+    const rhymeElements = await createRhymeElements(rhymes);
+    clearResultsElem();
+    populateResultsElem(rhymeElements);
   }
 }
 
-function clearResults() {
-  Array.from(resultDiv.childNodes).forEach((child) => {
-    child.remove;
+function searchForRhymes(query) {
+  const rhymeResults = fetch(
+    `https://rhymebrain.com/talk?function=getRhymes&word=${query}`
+  )
+    .then(function (responseFromEndpoint) {
+      return responseFromEndpoint.json();
+    })
+    .then(function (rhymeResultsJson) {
+      const truncatedTo10 = rhymeResultsJson.slice(0, 10);
+      console.log(truncatedTo10);
+      return truncatedTo10;
+    });
+  return rhymeResults;
+  // const rhymeResultsJson = await rhymeResults.json();
+  // const truncatedTo10 = rhymeResultsJson.slice(0, 10);
+  // console.log(truncatedTo10);
+  // return truncatedTo10;
+}
+
+async function createRhymeElements(rhymeResultsJson) {
+  const wordInfos = await getWordsInfos(rhymeResultsJson);
+
+  return rhymeResultsJson.map((rhymeWord, i) => {
+    let resultElem = document.createElement("div");
+    resultElem.classList.add("result");
+    resultElem.dataset.score = rhymeWord.score;
+    resultElem.append(rhymeWord.word);
+    resultElem.append(createWordInfoElements(wordInfos[i]));
+    resultElem = styleRhymeResult(resultElem);
+    return resultElem;
   });
 }
 
-async function search(query) {
-  const results = fetch(
-    `https://rhymebrain.com/talk?function=getRhymes&word=${searchBox.value}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (results) {
-      const truncated = results.slice(0, 10);
-      console.log(truncated);
-      return truncated;
-    });
-}
-
-async function createElements(results) {
-  const wordInfo = await getWordInfo(results);
-}
-
-async function getWordInfo(results) {
-  const wordInfo = await Promise.all(
-    results.map(async (rhyme) => {
+async function getWordsInfos(rhymes) {
+  const wordsInfos = await Promise.all(
+    rhymes.map(async (rhyme) => {
       const wordInfo = await fetch(
-        `https://rhymebrain.com/talk?function=getRhymes&word=${rhyme.word}`
+        `https://rhymebrain.com/talk?function=getWordInfo&word=${rhyme.word}`
       );
       const wordInfoJson = await wordInfo.json();
       return wordInfoJson;
     })
   );
-  return wordInfo;
+  return wordsInfos;
+}
+
+function createWordInfoElements(wordInfo) {
+  const wordInfoElem = document.createElement("dl");
+  for (const [key, value] of Object.entries(wordInfo)) {
+    const dt = document.createElement("dt");
+    dt.append(key);
+    const dd = document.createElement("dd");
+    dd.append(value);
+    wordInfoElem.append(dt);
+    wordInfoElem.append(dd);
+  }
+  return wordInfoElem;
+}
+
+function styleRhymeResult(resultElem) {
+  const styledResult = resultElem;
+  const resultScore = parseInt(resultElem.dataset.score, 10);
+  styledResult.style.fontSize = `${0.5 + (3.5 * resultScore) / 300}rem`;
+  return styledResult;
+}
+
+function clearResultsElem() {
+  Array.from(resultsContainerElem.childNodes).forEach((child) => {
+    child.remove();
+  });
+}
+
+function populateResultsElem(rhymeResultsElems) {
+  resultsContainerElem.append(...rhymeResultsElems);
 }
